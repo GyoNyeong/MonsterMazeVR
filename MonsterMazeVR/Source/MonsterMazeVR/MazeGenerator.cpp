7e2452b625b8d31f2e3b373664430de12e94219c
museongchoi <antjd3303@hs.ac.kr>
2024-09-17 20:41:30 +0900

    CMS : feat(MazeGenerator.cpp/GenerateMaze, SpawnBlock) Add Feature RandomMaze | Add Feature SpawnBlock
    GenerateMaze
    미로 기본 구조 생성
    - PlayerStart & ExitPortal 생성
    미로 무작위로 벽 추가
    - y 방향을 기준으로 & x 방향을 기준으로 벽 추가
    
    SpawnBlock
    특정 위치와 회전값을 기반으로 미로의 블록(벽, 바닥, 출구 등) 을 스폰하는 역할.

--- MonsterMazeVR/Source/MonsterMazeVR/MazeGenerator.cpp
+++ MonsterMazeVR/Source/MonsterMazeVR/MazeGenerator.cpp
@@ -2,6 +2,7 @@
 
 
 #include "MazeGenerator.h"
+#include "Wall.h"
 
 const int MazeSizeMax = 101;
 
@@ -34,6 +35,7 @@ void AMazeGenerator::PostEditChangeProperty(struct FPropertyChangedEvent& e)
 void AMazeGenerator::BeginPlay()
 {
 	Super::BeginPlay();
+	GenerateMaze(SizeX, SizeY);
 	
 }
 
@@ -44,9 +46,11 @@ void AMazeGenerator::Tick(float DeltaTime)
 
 }
 
+// 미로 생성 핵심 함수
 void AMazeGenerator::GenerateMaze(const int TileX, const int TileY)
 {
 	// 미로 크기 처리 : 미로 크기가 짝수이면 오류, 미로 크기가 홀수 크기에서만 생성.
+	// only odd numbers allowed
 	if (TileX % 2 == 0 || TileY % 2 == 0)
 	{
 		if (GEngine)
@@ -64,10 +68,11 @@ void AMazeGenerator::GenerateMaze(const int TileX, const int TileY)
 
 	// 변수 초기화
 	float CaptureX = 0.0f;
-	float CpatureY = 0.0f;
+	float CaptureY = 0.0f;
 	const float Offset = 350.0f;
 
 	// 기존 PlayerStart 와 ExitPortal 제거
+	// Destroy already spawned PlayerStart and ExitPortal if sapwned in editor
 	if (SpawnedPlayerStart != nullptr)
 	{
 		SpawnedPlayerStart->Destroy();
@@ -78,29 +83,130 @@ void AMazeGenerator::GenerateMaze(const int TileX, const int TileY)
 	}
 
 	// 미로 초기화
+	// Init Maze
 	MazeGrid.Clear();
 	MazeGrid.AddUninitialized(TileX, TileY);
 
 	// 미로의 기본 구조 생성
+	// this builds outer walls and the initial symmetric grid structure filled with walls separated equally by ground blocks
 	for (int x = 0; x < TileX; x++)
 	{
 		for (int y = 0; y < TileY; y++)
 		{
-			const FVector Location(CaptureX, CpatureY, 0.0f);
+			const FVector Location(CaptureX, CaptureY, 0.0f);
 			if (y == 0 || x == 0 || y == TileY - 1 || x == TileX - 1 || y % 2 == 0 && x % 2 == 0)
 			{
 				MazeGrid.Rows[x].Columns[y] = SpawnBlock(Wall, Location);
+			}
+			else
+			{
+				MazeGrid.Rows[x].Columns[y] = SpawnBlock(Ground, Location);
+			}
+
+			// spawn PlayerStart
+			if (CaptureX == Offset && CaptureY == Offset)
+			{
+				const auto CenterBlockLocation = FVector(Location.X + (Offset / 2), Location.Y + (Offset / 2), Location.Z + (Offset / 2));
+				SpawnedPlayerStart = SpawnBlock(PlayerStart, CenterBlockLocation);
+			}
+			// spawn ExitPortal
+			if (y == TileY - 1 && x == TileX - 1)
+			{
+				const auto CenterBlockLocation = FVector(Location.X - (Offset / 2), Location.Y - (Offset / 2), Location.Z + (Offset / 2));
+				SpawnedExitPortal = SpawnBlock(ExitPortal, CenterBlockLocation);
+			}
+
+			CaptureY += Offset;
+			if (CaptureY >= Offset * TileY)
+			{
+				CaptureY = 0;
+			}
+		}
+		CaptureX += Offset;
+		if (CaptureX >= Offset * TileX)
+		{
+			CaptureX = 0;
+		}
+	}
+	
+	// 미로 무작위로 벽 추가
+	// this adds walls to the existing grid to from random corridors
+	for (int y = 2; y < TileY - 1; y += 2)
+	{
+		int NextX = 2;
+		int NextY = y;
+		// int random4
+
+		switch (rand() % 4)
+		{
+		case 0: NextX++;
+			break;
+		case 1: NextX--;
+			break;
+		case 2: NextY++;
+			break;
+		case 3: NextY--;
+		default:
+			break;
+		}
+
+		if (!MazeGrid.Rows[NextX].Columns[NextY]->IsA(AWall::StaticClass()))
+		{
+			ReplaceBlock(Wall, NextX, NextY);
+		}
+		else
+		{
+			y -= 2;
+		}
+	}
+	for (int x = 4; x < TileX - 1; x += 2)
+	{
+		for (int y = 2; y < TileY - 1; y += 2)
+		{
+			int NextX = x;
+			int NextY = y;
+
+			switch (rand() % 3)
+			{
+			case 0: NextY++;
+				break;
+			case 1: NextY--;
+				break;
+			case 2: NextX++;
+				break;
+			default:
+				break;
+			}
+
+			if (!MazeGrid.Rows[NextX].Columns[NextY]->IsA(AWall::StaticClass()))
+			{
+				ReplaceBlock(Wall, NextX, NextY);
+			}
+			else
+			{
+				y -= 2;
 			}
 		}
 	}
 }
 
-AActor* AMazeGenerator::SpawnBlock(UClass* BlockType, FVector Locatiom, FRotator Rotation)
+AActor* AMazeGenerator::SpawnBlock(UClass* BlockType, const FVector Location, const FRotator Rotation)
 {
-	return nullptr;
+	if (BlockType == nullptr)
+	{
+		return nullptr;
+	}
+	AActor* NewBlock = GetWorld()->SpawnActor<AActor>(BlockType, Location, Rotation);
+
+	#if WITH_EDITOR
+	NewBlock->SetFolderPath("/Maze");
+	#endif
+
+	return NewBlock;
 }
 
 void AMazeGenerator::ReplaceBlock(UClass* NewBlock, int MazeX, int MAzeY)
 {
+	
 }
 
