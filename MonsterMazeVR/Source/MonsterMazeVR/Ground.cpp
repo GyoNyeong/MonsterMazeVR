5564aaae025d0878f67560b53a8c61595d893636
museongchoi <antjd3303@hs.ac.kr>
2024-09-19 09:47:22 +0900

    CMS : feat(Ground.cpp) Add feature Mesh
    Mesh
    - 메쉬 컴포넌트 생성 & 이름 설정
    - 메쉬 컴포넌트에 쿼리와 물리 충돌을 활성화
    - 액터의 루트 컴포넌트를 메쉬로 설정
    (즉, 액터의 트랜스폼은 Mesh를 기준으로 이루어짐.)

--- MonsterMazeVR/Source/MonsterMazeVR/Ground.cpp
+++ MonsterMazeVR/Source/MonsterMazeVR/Ground.cpp
@@ -9,6 +9,10 @@ AGround::AGround()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	RootComponent = Mesh;
+
 }
 
 // Called when the game starts or when spawned
