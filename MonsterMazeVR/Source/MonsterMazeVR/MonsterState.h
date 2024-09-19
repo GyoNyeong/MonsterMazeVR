42affb3323248cbe7f6b59b448a175d114bfbe2e
GyoNyeong <rysud123@naver.com>
2024-09-20 00:52:57 +0900

    GN:Plugin제거, c++ monsteraicontroller 작성,
    여전히 c++기반 monstercontroller 작동안됨. bp로 다 만듬. move to는 둘다 작동안됨. move directly는 bp기반에선 작동

---
+++ MonsterMazeVR/Source/MonsterMazeVR/MonsterState.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "MonsterState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONSTERMAZEVR_API UMonsterState : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+};
+
+UENUM()
+enum class EMonsterState
+{
+	Stand UMETA(DisplayName = "Stand"),
+	Chase UMETA(DisplayName = "Chase"),
+	Attack UMETA(DisplayName = "Attack"),
+	Die UMETA(DisplayName = "Die")
+};
