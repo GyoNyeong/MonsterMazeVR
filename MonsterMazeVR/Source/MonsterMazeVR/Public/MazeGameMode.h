e3e64f4279711935cdccdc5badee3356c2dc9cad
museongchoi <antjd3303@hs.ac.kr>
2024-09-19 12:25:55 +0900

    CMS : feat(MazeGameMode.h) Add feature MazeGemaMode
    1. TArray 배열 생성
    2. CurrentLevel, MazecompletionTimerHandle, TotalSecondmazecompletion, CurrentLevelIndex 변수 생성
    3. Fun 생성
    - IncrementMazeCompletionTime
    - LevelComplete
    - startNextLevel

---
+++ MonsterMazeVR/Source/MonsterMazeVR/Public/MazeGameMode.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MazeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(minimalapi)
+class MONSTERMAZEVR_API AMazeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMazeGameMode();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = Levels)
+	TArray<TSubclassOf<AActor>> Levels;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	AActor* CurrentLevel;
+
+	FTimerHandle MazeCompletionTimerHandle;
+
+	int32 TotalSecondsMazeCompletion;
+
+	int32 CurrentLevelIndex;
+
+	void IncrementMazeCompletionTime();
+
+	UFUNCTION()
+	void LevelComplete();
+
+	void StartNextLevel();
+	
+};
