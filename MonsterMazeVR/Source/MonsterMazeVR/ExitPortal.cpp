ce53a5f6a943de073d36479c5c41a17b7e43b8be
museongchoi <antjd3303@hs.ac.kr>
2024-09-19 10:32:14 +0900

    CMS : feat(ExitPortal.cpp) Add Feature ExitPortal
    1. OverlapComponent UBoxComponent 생성
    - 포털에 대한 박스 콜리전
    - 박스 크기 설정
    2. OverlapComponent RootComponent 설정
    3. UStaticMeshComponent 생성 및 설정
    - 메쉬 설정
    - 충돌 비활성화 (충돌은 BoxComp 로 처리) 시각적 표현만 담당
    - 메쉬는 루트 컴포넌트(박스)와 연결, 포털 위치와 동일 유지
    4. OnComponentBeginOverlap 이벤트 바인딩
    - OnPortalBeginOverlap 함수를 BoxComponent 의 Overlap 이벤트에 바인딩

--- MonsterMazeVR/Source/MonsterMazeVR/ExitPortal.cpp
+++ MonsterMazeVR/Source/MonsterMazeVR/ExitPortal.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ExitPortal.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 AExitPortal::AExitPortal()
@@ -9,6 +10,15 @@ AExitPortal::AExitPortal()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	auto OverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	OverlapComponent->SetBoxExtent(FVector(75, 75, 50));
+	RootComponent = OverlapComponent;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Mesh->SetupAttachment(RootComponent);
+
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AExitPortal::OnPortalBeginOverlap);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +26,12 @@ void AExitPortal::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AExitPortal::OnPortalBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& sweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap Detected with %s"), *OtherActor->GetName());
+	LevelComplete.Broadcast();
 }
 
 // Called every frame
